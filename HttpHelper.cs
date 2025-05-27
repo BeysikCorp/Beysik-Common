03836cb2c9aeb885af6b6af424dae04f6a027dfe
Trent <65882951+Dre-OS@users.noreply.github.com>
2025-05-27 17:15:55 +0800

    added all common classes

---
+++ HttpHelper.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+using System.Text.Json;
+
+namespace Beysik_Common
+{
+    public class HttpHelper
+    {
+        private readonly HttpClient _httpClient;
+
+        public HttpHelper(HttpClient httpClient)
+        {
+            _httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
+        }
+
+        public async Task<T?> GetAsync<T>(string url)
+        {
+            var response = await _httpClient.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadAsStringAsync();
+            return JsonSerializer.Deserialize<T>(content);
+        }
+
+        public async Task<T?> PostAsync<T>(string url, object data)
+        {
+            var jsonContent = JsonSerializer.Serialize(data);
+            var content = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+
+            var response = await _httpClient.PostAsync(url, content);
+            response.EnsureSuccessStatusCode();
+
+            var responseContent = await response.Content.ReadAsStringAsync();
+            return JsonSerializer.Deserialize<T>(responseContent);
+        }
+
+        public async Task<T?> PutAsync<T>(string url, object data)
+        {
+            var jsonContent = JsonSerializer.Serialize(data);
+            var content = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+
+            var response = await _httpClient.PostAsync(url, content);
+            response.EnsureSuccessStatusCode();
+
+            var responseContent = await response.Content.ReadAsStringAsync();
+            return JsonSerializer.Deserialize<T>(responseContent);
+        }
+    }
+}
---
+++ RabbitMQHelper.cs
@@ -0,0 +1,65 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace Beysik_Common 
+{
+    public class RabbitMqHelper
+    {
+        private readonly string _hostName;
+        private readonly string _queueName;
+        private readonly IConnection _connection;
+        private readonly IModel _channel;
+        public string _message { get; set; } = string.Empty;
+
+        public RabbitMqHelper(string queueName)
+        {
+            _hostName = "192.168.191.216";
+            _queueName = queueName;
+
+            var factory = new ConnectionFactory() { HostName = _hostName };
+            _connection = factory.CreateConnection();
+            _channel = _connection.CreateModel();
+
+            _channel.QueueDeclare(queue: queueName, durable: true, exclusive: false, autoDelete: false, arguments: null);
+        }
+
+        public Task PublishMessage(string message)
+        {
+            var body = System.Text.Encoding.UTF8.GetBytes(message);
+
+            try
+            {
+                _channel.BasicPublish(exchange: string.Empty, routingKey: "hello", body: body);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            //Console.WriteLine($" [x] Sent {message}");
+            return Task.CompletedTask;
+        }
+
+        public Task ConsumeMessages()
+        {
+            var consumer = new EventingBasicConsumer(_channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                _message = System.Text.Encoding.UTF8.GetString(body);
+
+                //Console.WriteLine($" [x] Received {message}");
+            };
+            _channel.BasicConsume(queue: _queueName, autoAck: true, consumer: consumer);
+
+            return Task.CompletedTask;
+        }
+
+        public void CloseConnection()
+        {
+            _channel.Close();
+            _connection.Close();
+        }
+    }
+}
