388cae3db9ee84ced15c2334d8d8068a81ec1b66
Trent <hlsagabaen@gmail.com>
2025-05-30 11:59:57 +0800

    fixed initialization

--- RabbitMQHelper.cs
+++ RabbitMQHelper.cs
@@ -7,26 +7,34 @@ namespace Beysik_Common
     public class RabbitMqHelper
     {
         private readonly string _hostName;
-        private readonly string _queueName;
+        private string _queueName;
         private readonly IConnection _connection;
         private readonly IModel _channel;
         public string _message { get; set; } = string.Empty;
 
-        public RabbitMqHelper(string hostname, string queueName)
+        public RabbitMqHelper(string hostname)
         {
             _hostName = hostname;
-            _queueName = queueName;
 
-            var factory = new ConnectionFactory() { HostName = hostname, UserName="guest", Password="guest" };
+            var factory = new ConnectionFactory() { HostName = _hostName, UserName="guest", Password="guest" };
             _connection = factory.CreateConnection();
             _channel = _connection.CreateModel();
 
-            _channel.QueueDeclare(queue: queueName, durable: true, exclusive: false, autoDelete: false, arguments: null);
+            //_channel.QueueDeclare(queue: queueName, durable: true, exclusive: false, autoDelete: false, arguments: null);
+        }
+
+        public void EnsureQueueExists(string queueName)
+        {
+            // Ensure the queue exists, this will create it if it doesn't
+            _queueName = queueName;
+            
+            _channel.QueueDeclare(queue: _queueName, durable: true, exclusive: false, autoDelete: false, arguments: null);
         }
 
 
-        public Task PublishMessage(string message)
+        public Task PublishMessage(string queueName, string message)
         {
+            EnsureQueueExists(queueName);
             var body = System.Text.Encoding.UTF8.GetBytes(message);
 
             try
@@ -43,9 +51,9 @@ namespace Beysik_Common
             return Task.CompletedTask;
         }
 
-        public Task ConsumeMessages(string reqmessage)
+        public Task ConsumeMessages(string queueName, string reqmessage)
         {
-
+            EnsureQueueExists(queueName);
             var consumer = new EventingBasicConsumer(_channel);
             //var message = "";
             do
@@ -57,7 +65,7 @@ namespace Beysik_Common
 
                     //Console.WriteLine($" [x] Received {message}");
                 };
-                _channel.BasicConsume(queue: _queueName, autoAck: true, consumer: consumer);
+                 _channel.BasicConsume(queue: _queueName, autoAck: true, consumer: consumer);
             }
             while (!(reqmessage.Equals(_message)));
 
